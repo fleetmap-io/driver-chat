cf2bb75d8394eb1828ceacd6740bb754488ec329
jcardus <asklocation.net@gmail.com>
2023-07-10 12:43:36 +0100

    24 log error

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,9 @@
 export default async function ({ store, redirect }) {
-  try { await store.dispatch('fetchSession') } catch (e) { redirect('/login') }
+  try {
+    await store.dispatch('fetchSession')
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e)
+    redirect('/login')
+  }
 }
